8f35ca2085b55fc72475a109197e426a0d55476b
Shivananda Sai <shivanandasai.38@gmail.com>
2022-07-15 23:04:10 +0530

    5th video done

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { Typography, Grid, Grow } from "@mui/material";
+import { Typography, Grid, Grow, Rating, Tooltip } from "@mui/material";
 import useStyles from "./styles";
+import { Link } from "react-router-dom";
 
 const Movie = ({ movie, i }) => {
   const classes = useStyles();
   return (
     <Grid item xs={12} sm={6} md={4} ls={3} xl={2} className={classes.movie}>
       <Grow in key={i} timeout={(i + 1) * 250}>
-        <Typography className={classes.title} variant="h5">
-          {movie.title}
-        </Typography>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          <img
+            alt={movie.title}
+            className={classes.image}
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : `https://www.fillmurray.com/200/300`
+            }
+          />
+          <Typography className={classes.title} variant="h5">
+            {movie.title}
+          </Typography>
+          <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Link>
       </Grow>
     </Grid>
   );
