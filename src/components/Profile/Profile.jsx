eedc321f2875cdb15af3273171cf0f30b8a7b581
Shivananda Sai <shivanandasai.38@gmail.com>
2022-07-16 21:07:19 +0530

    finalizing authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { userSelector } from "../../features/auth";
+import { Typography, Button, Box } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
-  console.log("Profile", user);
-  return <div>Profile - {user.username}</div>;
+  const favoriteMovies = [];
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+  return (
+    <>
+      <Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="h4" gutterBottom>
+            My Profile
+          </Typography>
+          <Button color="inherit" onClick={logout}>
+            Logout &nbsp; <ExitToApp />
+          </Button>
+        </Box>
+        {!favoriteMovies.length ? (
+          <Typography variant="h5">
+            Add Favorites or watchlist some movies to see them here.
+          </Typography>
+        ) : (
+          <Box>FAVORITE MOVIES</Box>
+        )}
+      </Box>
+    </>
+  );
 };
 
 export default Profile;
