fd28cefda7a61bb61a1ca4e9ebd0298009fb91a5
Shivananda Sai <shivanandasai.38@gmail.com>
2022-07-16 18:21:02 +0530

    search functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,6 +18,7 @@ import { Link } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import useStyles from "./styles";
 import Sidebar from "../Sidebar/Sidebar";
+import Search from "../Search/Search";
 
 const NavBar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -44,7 +45,7 @@ const NavBar = () => {
           <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
             {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
-          {!isMobile && "Search..."}
+          {!isMobile && <Search />}
           <div>
             {!isAuthenticated ? (
               <Button color="inherit" onClick={() => {}}>
@@ -67,7 +68,7 @@ const NavBar = () => {
               </Button>
             )}
           </div>
-          {isMobile && "Search..."}
+          {isMobile && <Search />}
         </Toolbar>
       </AppBar>
       <div>
