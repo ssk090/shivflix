333b8c540c3967368f599379de24f1b52dba1cb7
Shivananda Sai <shivanandasai.38@gmail.com>
2022-07-16 19:23:52 +0530

    authentication solution

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/auth";
 
 const Profile = () => {
-  console.log("Profile");
-  return <div>Profile</div>;
+  const { user } = useSelector(userSelector);
+  console.log("Profile", user);
+  return <div>Profile - {user.username}</div>;
 };
 
 export default Profile;
