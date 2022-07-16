294482ff9575d4b2ae8b79d98418c2c2af46db52
Shivananda Sai <shivanandasai.38@gmail.com>
2022-07-17 01:33:00 +0530

    movie information -2

--- src/components/MovieInformation/style.js
+++ src/components/MovieInformation/style.js
@@ -46,4 +46,19 @@ export default makeStyles((theme) => ({
       padding: "0.5rem 1rem",
     },
   },
+  castImage: {
+    width: "100%",
+    maxWidth: "7em",
+    height: "8em",
+    objectFit: "cover",
+    borderRadius: "10px",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
 }));
